b336a494c8cdb410161353c714ee9d305466fa96
Beritz <61402532+Beritz@users.noreply.github.com>
2020-11-23 16:58:56 +0700

    Added Hello There

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public Main() {
 		System.out.println("owo");
 		System.out.println("what");
+		System.out.println("Hello There -Dodo");
 	}
 	public static void main(String[]Args) {
 		new Main();
