15a8c748299eff3a998b01397fbbd2143ae5e798
Henry Setiawan <henry_setiawan24@yahoo.co.id>
2020-11-24 17:51:07 +0700

    tambahin class User

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -2,10 +2,7 @@ package Main;
 
 public class Main {
 	public Main() {
-		System.out.println("owo");
-		System.out.println("what");
-		System.out.println("Hello There -Dodo");
-		System.out.println("why");
+		
 	}
 	public static void main(String[]Args) {
 		new Main();
