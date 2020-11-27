484b5834ba9d7754ecda8028bd40087d7e7140be
Beritz <61402532+Beritz@users.noreply.github.com>
2020-11-27 13:36:57 +0700

    ViewMenuNoBtn

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,10 +1,12 @@
 package Main;
 
 import View.MainMenu;
+import View.ViewJobs;
 
 public class Main {
 	public Main() {
-		new MainMenu();
+//		new MainMenu();
+		new ViewJobs();
 	}
 	public static void main(String[]Args) {
 		new Main();
